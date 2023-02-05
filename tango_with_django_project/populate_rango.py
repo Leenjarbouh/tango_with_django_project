a85922291b999bcef4ccb4cc6c9ffaf32997416a
Leen Jarbouh <2580367j@student.gla.ac.uk>
2023-02-05 21:38:48 +0000

    Chapter 6 done

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -60,7 +60,7 @@ def populate():
 def add_page(cat,title,url,views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
-    p.views=views
+    p.views=views +15
     p.save()
     return p
     
